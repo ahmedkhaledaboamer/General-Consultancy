dcf4921392255ed9e1b499337ebe5d1575f53dd2
Mohamed176b <moshafey18@gmail.com>
2026-03-17 10:48:17 +0400

    Add Rest Translations, Sitemap and Robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
 import { Cairo } from "next/font/google";
 import "./globals.css";
+import { getLocale } from "next-intl/server";
 const cairo = Cairo({
   variable: "--font-cairo",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
   return (
-    <html>
+    <html lang={locale}>
       <body className={`${cairo.className} antialiased`}>
         {children}
       </body>
